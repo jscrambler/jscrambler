53dcbb026ac98ca360321e21f26997860ad106e8
Michael Nicholls <nicholls.mc@gmail.com>
2018-03-07 13:16:59 +0000

    Fix output absolute path handling in grunt-jscrambler

--- packages/grunt-jscrambler/tasks/jscrambler.js
+++ packages/grunt-jscrambler/tasks/jscrambler.js
@@ -23,6 +23,10 @@ module.exports = function (grunt) {
     function writeFile(buffer, file) {
       files.forEach(function (elem) {
         elem.src.forEach(function (src) {
+          if(process.platform !== 'win32' && grunt.file.isPathAbsolute(src)) {
+            var parsedPath = path.parse(src);
+            src = src.replace(parsedPath.root, '');
+          }
           if (grunt.file.arePathsEquivalent(src, file)) {
             var dest = elem.dest;
             var lastDestChar = dest[dest.length - 1];
@@ -33,8 +37,6 @@ module.exports = function (grunt) {
               destPath = path.join(dest, file);
             }
             grunt.file.write(destPath, buffer);
-          } else if (elem.dest) {
-            grunt.file.write(path.join(elem.dest, file), buffer);
           }
         });
       });
