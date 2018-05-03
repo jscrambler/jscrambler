e05795ac8c0a0632ebf75d488f696eae3bfbb0d2
Cody Taylor <codemister99@yahoo.com>
2018-05-03 19:05:54 -0400

    Replace event-stream with through2.
    :heavy_minus_sign: event-stream@3.1.5
    :heavy_plus_sign: through2@2.0.3
    
    The event-stream module has outdated dependencies. The "through"
    method provides no callback, forcing events to fired manually.

--- packages/gulp-jscrambler/index.js
+++ packages/gulp-jscrambler/index.js
@@ -1,9 +1,9 @@
 'use strict';
 var defaults = require('lodash.defaults');
-var es = require('event-stream');
 var File = require('vinyl');
 var jScrambler = require('jscrambler').default;
 var path = require('path');
+var through = require('through2');
 
 module.exports = function (options) {
   options = defaults(options || {}, {
@@ -11,12 +11,13 @@ module.exports = function (options) {
   });
 
   var filesSrc = [];
-  var aggregate = function (file) {
+  var aggregate = function (file, enc, next) {
     if (file.contents) {
       filesSrc.push(file);
     }
+    next(null);
   };
-  var scramble = function () {
+  var scramble = function (done) {
     var self = this;
     jScrambler
       .protectAndDownload({
@@ -33,20 +34,18 @@ module.exports = function (options) {
       }, function (buffer, file) {
         var cwd = options && options.cwd || process.cwd();
         var relativePath = path.relative(cwd, file);
-        self.emit('data', new File({
+        self.push(new File({
           path: relativePath,
           contents: buffer
         }));
       })
       .then(function () {
-        self.emit('end');
+        done(null);
       })
       .catch(function (error) {
         // need to emit in nextTick to avoid the promise catching a re-thrown error
-        process.nextTick(function () {
-          self.emit('error', error);
-        });
+        process.nextTick(done, error);
       });
   };
-  return es.through(aggregate, scramble);
+  return through.obj(aggregate, scramble);
 };
