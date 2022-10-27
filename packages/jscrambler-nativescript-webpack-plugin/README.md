a421699f128f2c4efc3e1aaf2fafd4573280c58c
vitor.veiga <vitor.veiga@jscrambler.com>
2022-10-27 09:53:54 +0100

    fix(github-action): remove unnecessary folder and use private action

--- packages/jscrambler-nativescript-webpack-plugin/README.md
+++
@@ -1,3 +0,0 @@
-# Jscrambler NativeScript Webpack Plugin
-
-This metro plugin protects your **NativeScript** code using Jscrambler when bundled with Webpack.
--- packages/jscrambler-nativescript-webpack-plugin/lib/constants.js
+++
@@ -1,7 +0,0 @@
-const JSCRAMBLER_CLIENT_ID = 7;
-const JSCRAMBLER_IGNORE = '.jscramblerignore';
-
-module.exports = {
-  JSCRAMBLER_CLIENT_ID,
-  JSCRAMBLER_IGNORE
-};
