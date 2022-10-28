84324722f4955ec76665ac72fae2856bb1a7f563
Luis Reis <luiscubal@gmail.com>
2022-10-28 10:57:49 +0100

    feat: properly support config files

--- packages/jscrambler-github-action/src/index.ts
+++ packages/jscrambler-github-action/src/index.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core';
+import defaults from 'lodash.defaults';
+import {readFile} from 'fs/promises';
 import getInputs from './get-inputs';
 import setOutputs from './set-outputs';
 
@@ -8,6 +10,17 @@ const jscrambler = require('jscrambler').default;
 async function launch() {
   // Simply read and log the application ID for testing purposes
   const params = getInputs();
+
+  let finalParams: any;
+  if (params.jscramblerConfigPath !== undefined) {
+    // Explicit parameters take precedence over config-specified parameters
+    // Note: require(...) wouldn't work here -- ncc doesn't allow dynamically loading modules
+    const configFile = await readFile(params.jscramblerConfigPath, 'utf-8');
+    const configObject = JSON.parse(configFile);
+    finalParams = defaults(params, configObject);
+  } else {
+    finalParams = params;
+  }
 
   const protectionId = await jscrambler.protectAndDownload(params);
 
