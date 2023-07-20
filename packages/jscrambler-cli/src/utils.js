e95f53e252cf05267a1dc9dc03e430b6914b0a8f
vitorveiga <vitor.veiga@jscrambler.com>
2023-07-20 15:17:46 +0100

    chore(dependencies): bump axios and glob

--- packages/jscrambler-cli/src/utils.js
+++ packages/jscrambler-cli/src/utils.js
@@ -1,4 +1,4 @@
-import glob from 'glob';
+import {globSync} from 'glob';
 import fs from 'fs';
 
 /**
@@ -7,7 +7,7 @@ import fs from 'fs';
  * @returns {string[]}
  */
 export function getMatchedFiles(pattern) {
-  let matchedFiles = glob.sync(pattern, {
+  let matchedFiles = globSync(pattern, {
     dot: true
   });
 
