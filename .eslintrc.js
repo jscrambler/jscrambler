ab1b470020f175bb34d339f69b3d57ae28367d63
Luis Reis <luiscubal@gmail.com>
2024-01-12 10:24:09 +0000

    fix: eslint tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
   extends: ['airbnb', 'prettier'],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  rules: {
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': ['error'],
+  },
+  plugins: ['prettier'],
 };
